468b4be1fe1a1b997797d2aa24374b7c8059625e
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-04-15 17:52:59 +0200

    feat: add database connection and implement basic movie routes

--- consegna.md
+++ consegna.md
@@ -3,7 +3,7 @@
 è ora di mettere alla prova le vostre conoscenze iniziando a costruire la vostra prima app completa! Ecco i primi step
 
 1. ~~Utilizzando il file in allegato, creiamo un database con MySQL Workbench~~
-2. Creiamo una nuova applicazione Express
+2. ~~Creiamo una nuova applicazione Express~~
 3. Colleghiamo l’app al db e verifichiamo che tutto funzioni
 4. Prepariamo una rotta index per ottenere la lista dei film
 5. Prepariamo una rotta show per ottenere i dettagli di un singolo film e le sue recensioni
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const cors = require('cors');
+const connection = require('./database/db,js');
 
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors({ origin: process.env.FRONT_URL || 'http://localhost:5173' }));
 app.use(express.json());
+app.use(express.static('public'));
 
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello, Movies!');
 });
@@ -11,3 +18,29 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
+//index route
+app.get('/movies', (req, res) => {
+    res.json({ message: `List of movies` })
+})
+
+//show route
+app.get('/movies/:id', (req, res) => {
+    const { id } = req.params
+    res.json({ message: `List of movies with id ${id}` })
+})
+
+
+
+
+
+// Error Middlewares
+app.use((req, res, next, err) => {
+    console.error(err.stack)
+    res.status(500).json({ message: 'Something Broke!' })
+})
+
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Sorry, that route doesn\'t exist!' })
+})
