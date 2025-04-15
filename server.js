c26967927f2436e998866c0fed73f7a6823eada6
Leonardo-Mocchi <leonardo.mocchi@gmail.com>
2025-04-15 17:54:12 +0200

    fix: correct typo in database connection file path

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const connection = require('./database/db,js');
+const connection = require('./database/db.js');
 
 const PORT = process.env.PORT || 3000;
 
